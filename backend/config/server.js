f70373c303c2299bf25554e4b6950d757d4b7a77
daunJung <dkun9039@paycrux.co.kr>
2021-11-05 16:41:29 +0900

    Initialize - strapi & nextjs 초기화

---
+++ backend/config/server.js
@@ -0,0 +1,9 @@
+module.exports = ({ env }) => ({
+  host: env('HOST', '0.0.0.0'),
+  port: env.int('PORT', 1337),
+  admin: {
+    auth: {
+      secret: env('ADMIN_JWT_SECRET', '97d8ebc08c37d52bd4f24765bdd926cb'),
+    },
+  },
+});
---
+++ backend/extensions/users-permissions/config/jwt.js
@@ -0,0 +1,3 @@
+module.exports = {
+  jwtSecret: process.env.JWT_SECRET || 'a268cd23-9ff4-45f4-af74-203102325190'
+};
